ab45e4bf237a4480be5b57cfa1708f4676f785f8
EzatRashad <ezatrashad2003@gmail.com>
2025-04-06 13:20:28 +0200

    Add CartModel and related classes for cart functionality

---
+++ lib/features/my_cart/data/model/CartModel.dart
@@ -0,0 +1,130 @@
+/// status : "success"
+/// message : "Product added successfully to your cart"
+/// numOfCartItems : 1
+/// cartId : "67f2606e407a95607a8657b3"
+/// data : {"_id":"67f2606e407a95607a8657b3","cartOwner":"67d2cf3affa203990fb42659","products":[{"count":2,"_id":"67f2606e407a95607a8657b4","product":"6428eb43dc1175abc65ca0b3","price":149}],"createdAt":"2025-04-06T11:07:26.434Z","updatedAt":"2025-04-06T11:07:30.668Z","__v":0,"totalCartPrice":298}
+
+class CartModel {
+  CartModel({
+      this.status, 
+      this.message, 
+      this.numOfCartItems, 
+      this.cartId, 
+      this.data,});
+
+  CartModel.fromJson(dynamic json) {
+    status = json['status'];
+    message = json['message'];
+    numOfCartItems = json['numOfCartItems'];
+    cartId = json['cartId'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+  String? status;
+  String? message;
+  num? numOfCartItems;
+  String? cartId;
+  Data? data;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = status;
+    map['message'] = message;
+    map['numOfCartItems'] = numOfCartItems;
+    map['cartId'] = cartId;
+    if (data != null) {
+      map['data'] = data?.toJson();
+    }
+    return map;
+  }
+
+}
+
+/// _id : "67f2606e407a95607a8657b3"
+/// cartOwner : "67d2cf3affa203990fb42659"
+/// products : [{"count":2,"_id":"67f2606e407a95607a8657b4","product":"6428eb43dc1175abc65ca0b3","price":149}]
+/// createdAt : "2025-04-06T11:07:26.434Z"
+/// updatedAt : "2025-04-06T11:07:30.668Z"
+/// __v : 0
+/// totalCartPrice : 298
+
+class Data {
+  Data({
+      this.id, 
+      this.cartOwner, 
+      this.products, 
+      this.createdAt, 
+      this.updatedAt, 
+      this.v, 
+      this.totalCartPrice,});
+
+  Data.fromJson(dynamic json) {
+    id = json['_id'];
+    cartOwner = json['cartOwner'];
+    if (json['products'] != null) {
+      products = [];
+      json['products'].forEach((v) {
+        products?.add(Products.fromJson(v));
+      });
+    }
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    v = json['__v'];
+    totalCartPrice = json['totalCartPrice'];
+  }
+  String? id;
+  String? cartOwner;
+  List<Products>? products;
+  String? createdAt;
+  String? updatedAt;
+  num? v;
+  num? totalCartPrice;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['_id'] = id;
+    map['cartOwner'] = cartOwner;
+    if (products != null) {
+      map['products'] = products?.map((v) => v.toJson()).toList();
+    }
+    map['createdAt'] = createdAt;
+    map['updatedAt'] = updatedAt;
+    map['__v'] = v;
+    map['totalCartPrice'] = totalCartPrice;
+    return map;
+  }
+
+}
+
+/// count : 2
+/// _id : "67f2606e407a95607a8657b4"
+/// product : "6428eb43dc1175abc65ca0b3"
+/// price : 149
+
+class Products {
+  Products({
+      this.count, 
+      this.id, 
+      this.product, 
+      this.price,});
+
+  Products.fromJson(dynamic json) {
+    count = json['count'];
+    id = json['_id'];
+    product = json['product'];
+    price = json['price'];
+  }
+  num? count;
+  String? id;
+  String? product;
+  num? price;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['count'] = count;
+    map['_id'] = id;
+    map['product'] = product;
+    map['price'] = price;
+    return map;
+  }
+
+}
